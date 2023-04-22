a9c690ab137c7882b9e71681561c71025b8cd1b4
VeronicaLissethMarin <veronica.marin@catolica.edu.sv>
2023-04-22 09:00:06 -0600

    Ingreso de acceso a base de datos

---
+++ L01P02_2020MS650_2020GG602/Models/restauranteDbContext.cs
@@ -0,0 +1,7 @@
+﻿namespace L01P02_2020MS650_2020GG602.Models
+{
+    public class restauranteDbContext
+    {
+
+    }
+}
--- L01P02_2020MS650_2020GG602/Program.cs
+++ L01P02_2020MS650_2020GG602/Program.cs
@@ -1,8 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using L01P02_2020MS650_2020GG602.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<restauranteDbContext>(opt =>
+                             opt.UseSqlServer(
+                              builder.Configuration.GetConnectionString("restauranteDbConnection"))
+);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
