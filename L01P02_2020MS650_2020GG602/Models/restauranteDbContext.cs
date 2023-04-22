9c0695f24610169218be5871cea71aeaf4669672
VeronicaLissethMarin <veronica.marin@catolica.edu.sv>
2023-04-22 09:09:23 -0600

    Ingresando de nuevo dbcontext

--- L01P02_2020MS650_2020GG602/Models/restauranteDbContext.cs
+++ L01P02_2020MS650_2020GG602/Models/restauranteDbContext.cs
@@ -1,7 +1,16 @@
-﻿namespace L01P02_2020MS650_2020GG602.Models
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace L01P02_2020MS650_2020GG602.Models
 {
     public class restauranteDbContext
     {
+        public DbSet<motoristas> motorista { get; set; }
+
+        public DbSet<pedidos> pedidos { get; set; }
+
+        public DbSet<platos> platos { get; set; }
+
+        public DbSet<clientes> clientes { get; set; }
 
     }
 }
