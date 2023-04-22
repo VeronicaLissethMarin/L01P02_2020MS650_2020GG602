83afad283948ce492a727a72971fdcb4a906de7d
VeronicaLissethMarin <veronica.marin@catolica.edu.sv>
2023-04-22 08:41:21 -0600

    Add project files.

---
+++ L01P02_2020MS650_2020GG602/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace L01P02_2020MS650_2020GG602.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
