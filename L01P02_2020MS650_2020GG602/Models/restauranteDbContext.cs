4295d6d65b990a23c9a47f7125311eda89f81455
VeronicaLissethMarin <veronica.marin@catolica.edu.sv>
2023-04-22 09:13:52 -0600

    Corrigiendo context

--- L01P02_2020MS650_2020GG602/Models/restauranteDbContext.cs
+++ L01P02_2020MS650_2020GG602/Models/restauranteDbContext.cs
@@ -2,8 +2,12 @@
 
 namespace L01P02_2020MS650_2020GG602.Models
 {
-    public class restauranteDbContext
+    public class restauranteDbContext: DbContext
     {
+        public restauranteDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
         public DbSet<motoristas> motorista { get; set; }
 
         public DbSet<pedidos> pedidos { get; set; }
