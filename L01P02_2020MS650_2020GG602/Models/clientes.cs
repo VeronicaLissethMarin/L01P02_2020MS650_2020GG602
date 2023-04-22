868eb94d4b9234e68957b60d04311b670747193f
Diego <diego.guevara@catolica.edu.sv>
2023-04-22 08:54:32 -0600

    Agregado de modelos

---
+++ L01P02_2020MS650_2020GG602/Models/clientes.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace L01P02_2020MS650_2020GG602.Models
+{
+    public class clientes
+    {
+        [Key]
+        public int clienteId { get; set; }
+        public string? nombreCliente { get; set; }
+        public string? direccion { get; set; }
+
+    }
+}
